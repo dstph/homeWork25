78ab62d28edb7c1c537c77964637dabf8761532f
dstph <genyadstph@gmail.com>
2020-09-17 15:29:27 +0400

    done

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <FormGenerator :form="['input[type=text]', 'input[type=email]', 'input[type=button]', 'input[type=text]']" :model="inputData" />
+    <p>{{inputData}}</p>
+  </div>
+</template>
+
+<script>
+import FormGenerator from './components/FormGenerator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FormGenerator
+  },
+  data(){
+    return{
+      inputData: []
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/FormGenerator.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="form">
+		<input v-for="(item, index) in form" :key="item.index" :id="index" :type="item.substr(11).replace(']', '')" v-model="model[index]" >	
+		<span></span>
+    </div>
+</template>
+
+<script>
+	export default{
+		props: ['form','model']
+	}
+</script>
+
+<style>
+input{
+	display: block;
+	margin: 5px;
+}
+
+</style>
+	
+
